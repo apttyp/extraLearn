e2cf9a764aea71a530b8a480853cb391c9225120
apttyp <18112619045@163.com>
2017-02-07 20:59:42 +0800

    blackboard_one_edit

--- blackBoard_webCrawler/black_one.py
+++ blackBoard_webCrawler/black_one.py
@@ -3,11 +3,29 @@
 import requests
 import re
 url = 'http://www.heibanke.com/lesson/crawler_ex00/'
-
 r = requests.get(url)
-# print r.text
 res = r.text
-# print res
 p = re.compile('<h3>.*</h3>')
 for content in p.findall(res):
     print content
+    print content.__class__
+    mm = re.findall(r"\d\d\d\d\d", content)
+    print mm.__str__()
+    num = "".join(mm)
+    tmpurl = url + num
+    print tmpurl
+x = 1
+while x <= 20:
+    print x
+    x = x + 1
+    r = requests.get(tmpurl)
+    res = r.text
+    p = re.compile('<h3>.*</h3>')
+    for content in p.findall(res):
+        print content
+        print content.__class__
+        mm = re.findall(r"\d\d\d\d\d", content)
+        print mm.__str__()
+        num = "".join(mm)
+        tmpurl = url+num
+        print tmpurl
