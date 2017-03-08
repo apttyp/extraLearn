4a6e076d8ea4932680c614799d45f4a7b92a3905
apttyp <18112619045@163.com>
2017-03-08 21:55:39 +0800

    get_pic_from_tieba

---
+++ capture_tieba/Gakki_get.py
@@ -0,0 +1,29 @@
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+localDir = "C:\\tiebaPic\\"
+
+def getContent(url):
+
+    html = urllib.urlopen(url)
+    content = html.read()
+    html.close()
+    return content
+
+def getImage(info):
+    soup = BeautifulSoup(info, "html.parser")
+    imgAll = soup.find_all('img', class_="BDE_Image")
+    print imgAll
+    x=1
+    for img in imgAll:
+        url_img = img['src']
+        print localDir + '%s.jpg' %x
+        # urllib.urlretrieve()
+        urllib.urlretrieve(url_img, localDir + '%s.jpg' % (x))
+        x=x+1
+
+
+url = 'http://tieba.baidu.com/p/4949098493'
+info=getContent(url)
+# print info
+print getImage(info)
