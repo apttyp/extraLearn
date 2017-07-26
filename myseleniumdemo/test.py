905ea39276f4d490e54f3ce0f8ea621a5839f764
apttyp <18112619045@163.com>
2017-07-26 14:07:32 +0800

    modify

--- myseleniumdemo/test.py
+++ myseleniumdemo/test.py
@@ -2,9 +2,9 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import logging
+#import logging
 
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 options = webdriver.ChromeOptions()
 options.add_argument('lang=zh_CN.UTF-8')
@@ -12,12 +12,9 @@ options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(chrome_options = options)
 driver.get('https://www.baidu.com')
 time.sleep(5)
-print driver.find_element_by_xpath('//*[@id="main-message"]/div[2]').text()
-#driver.find_element_by_xpath('//*[@id="main-content"]').send_keys(Keys.SPACE)
+print driver.find_element_by_xpath('//*[@id="lg"]')
 time.sleep(3)
-#driver.find_element_by_xpath('//*[@id="main-content"]').send_keys(Keys.SPACE)
-
-#driver.get("http://www.youdao.com")
-#driver.find_element_by_name("q").send_keys("hello")
-#driver.find_element_by_name("q").send_keys("key.ENTER")
-#driver.close()
+for image in driver.find_elements_by_tag_name("img"):
+    print (image.text)
+    print (image.size)
+    print (image.tag_name)
